5d901c1d538af7a0d2049b48fb7a2a0938b6ccb6
Paige Wiener <snowplushie@yahoo.com>
2019-02-27 18:54:04 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(language)
-  puts "Hi, my name is .introduction and I am learning to program in #{language}"
+  puts "Hi, my name is #{introduction} and I am learning to program in #{language}"
 end
